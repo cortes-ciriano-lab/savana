6fe3e6ee2db49dbc44b3009f643cabbf8e4b2c8e
Seongmin Choi <soymintc@gmail.com>
2023-03-13 15:27:19 -0400

    fix four space chars into one tab char

--- savana/core.py
+++ savana/core.py
@@ -191,9 +191,9 @@ class ConsensusBreakpoint():
 			gt_tag = '0/0'
 		else:
 			gt_tag = '0/1'
-        support_str = ';'.join([f'{label.upper()}_SUPPORT={label_count}' for label, label_count in self.support.items()])
+		support_str = ';'.join([f'{label.upper()}_SUPPORT={label_count}' for label, label_count in self.support.items()])
 		stats_str = self.get_stats_str()
-        info = [f'{support_str};SVLEN={self.sv_length};BP_NOTATION={self.breakpoint_notation};{stats_str}']
+		info = [f'{support_str};SVLEN={self.sv_length};BP_NOTATION={self.breakpoint_notation};{stats_str}']
 		if self.breakpoint_notation == "<INS>":
 			info[0] = f'SVTYPE=INS;INSSEQ={self.inserted_sequence};' + info[0]
 		else:
